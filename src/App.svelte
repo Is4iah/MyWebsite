05ae2c93cf5d047ac0913b133a48d153cd888071
Isaias Andablo <andablo@usc.edu>
2022-06-27 13:34:14 -0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,75 @@
 <script>
-	export let name;
+	let name = 'my friends';
+    const channel = '<b>for now this is empty but in a couple of days this will look awesome :D<br></br></b>'
+    const headingID = 'heading'
+    const id = 'headingId'
+    let words = "<b> COLORS!! </b>"
+    // const disabled = false
+    let status = true
+    let promoted = false
+
+    let br = "<br></br>"
+
+    function changeStatus() {
+        if (promoted == false)  {
+            promoted = true;
+        }
+        else {
+            promoted = false;
+        }
+        if (status == false)  {
+            status = true;
+        }
+        else {
+            status = false;
+        }
+    }
+
+    
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <!-- <h2 class='underline'> Underlined text </h2> -->
+    <!-- <h2 {id}> {headingID} </h2>  -->
+    <!-- <h2 class={!promoted ? 'promoted' : ''}> Promoted </h2>
+    <h2 class:promoted> Promoted </h2> -->
+
+
+
+
+	<h1>{@html br}Hello {name}!</h1>
+    <div>{@html channel}</div> 
+    <h2 class={status ? 'win' : 'danger'} > AWESOME {@html br} </h2> 
+
+    <button on:click={changeStatus} class='button'> {@html words} </button>
+
+
 </main>
 
 <style>
+    .underline{ 
+        text-decoration: underline;
+    }
+
+    .danger { 
+        color: red;
+    }
+
+    .button{
+        color: purple;
+    }
+
+    .win {
+        color: blue;
+        text-decoration: underline;
+    }
+
+    .promoted{
+        font-style: italic;
+    }
+
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -16,7 +78,7 @@
 	}
 
 	h1 {
-		color: #ff3e00;
+		color: #00d0ff;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
